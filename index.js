2beb245b95466bce0a1e3d05c401a9e7f6010e52
andrew dillon <majik895@gmail.com>
2019-08-06 16:15:39 -0400

    added get

--- index.js
+++ index.js
@@ -7,3 +7,54 @@ const Users = require('./data/db.js');
 const server = express();
 
 server.use(express.json());
+
+server.post('/api/users', (req, res) => {
+    const { name, bio } = req.body;
+
+    if (!name || !bio) {
+        res
+            .status(400)
+            .json({ errorMessage: 'Yo dog provide name and bio for the user.' });
+    } else {
+        Users.insert(req.body)
+            .then(user => {
+                res.status(201).json(user);
+            })
+            .catch(() => {
+                res.status(500).json({
+                    errorMessage:
+                        'RuttRoh there was an error dog.',
+                });
+            });
+    }
+});
+
+server.get('/api/users', (req, res) => {
+    Users.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(() => {
+            res.status(500).json({
+                errorMessage: 'The users information could not be retrieved.',
+            });
+        });
+});
+
+server.get('/api/users/:id', (req, res) => {
+    Users.findById(req.params.id)
+        .then(user => {
+            if (user) {
+                res.status(200).json(user);
+            } else {
+                res
+                    .status(404)
+                    .json({ message: 'The user with the specified ID does not exist.' });
+            }
+        })
+        .catch(() => {
+            res
+                .status(500)
+                .json({ errorMessage: 'The user information could not be retrieved.' });
+        });
+});
